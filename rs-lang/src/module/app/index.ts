b2899e6e058b7faa60d3933466cb1b1ef5b83639
Geobo <bozhys@gmail.com>
2022-08-28 22:26:26 +0300

    refactor: send game to module

--- rs-lang/src/module/app/index.ts
+++ rs-lang/src/module/app/index.ts
@@ -7,8 +7,7 @@ import Render from '../ui';
 //Utils
 import getHTMLElement from '../../utils/getHTMLElement';
 import getHTMLInputElement from '../../utils/getHTMLInputElement';
-import getHTMLImageElement from '../../utils/getHTMLImageElement';
-import { getRandom, shuffle } from '../../utils/helpers';
+import { getRandom } from '../../utils/helpers';
 
 //Interface
 import IUserBody from '../interface/IUserBody';
@@ -35,6 +34,7 @@ import ModalLogin from '../login';
 
 //Games
 import Sprint from '../sprint';
+import AudioCall from '../audio-call';
 
 //State
 import State from './state';
@@ -108,7 +108,6 @@ export default class App {
                 Render.currentLink(req.path);
             })
             .get('/games/sprint/:group/:page', (req) => {
-                //console.log(req.path.split('/').reverse());
                 const group = Number(req.params.group);
                 const page = Number(req.params.page);
                 this.showSprint(group, page);
@@ -331,133 +330,8 @@ export default class App {
     }
 
     async showAudioCall(group: number, page: number) {
-        const main = getHTMLElement(document.querySelector('.main'));
-        main.innerHTML = '';
-        const game = this.render.gameAudioCall();
-        main.append(game);
-
-        let count = 0;
-        let attempt = 4;
-
-        const words = await this.data.getWords(group, page);
-        if (typeof words === 'number') {
-            console.log(`error ${words}`);
-            return;
-        }
-        shuffle(words);
-        this.showQuestion(words, count);
-
-        const answerBtns = document.querySelectorAll('.audio__choice, .audio__know-btn');
-        answerBtns.forEach((btn) => {
-            btn.addEventListener('click', (e) => {
-                const target = getHTMLElement(e.target);
-
-                this.showAnswer(target);
-                const answer = target.dataset.answer;
-
-                if (answer === undefined) {
-                    const hearts = document.querySelectorAll('.audio__icon-heart');
-                    hearts[attempt].classList.add('audio__icon-heart_miss');
-                    attempt -= 1;
-                }
-            });
-        });
-
-        const nextBtn = getHTMLElement(document.querySelector('.audio__next-btn'));
-        nextBtn.addEventListener('click', () => {
-            const len = words.length - 1;
-            if (count === len || attempt === -1) {
-                this.showResult();
-                return;
-            }
-            count += 1;
-            this.showQuestion(words, count);
-            this.hideAnswer();
-        });
-
-        const playBtn = document.querySelectorAll('.js-play-word');
-        playBtn.forEach((btn) => {
-            btn.addEventListener('click', (e) => {
-                let target = getHTMLElement(e.target);
-                if (!target.classList.contains('js-play-word')) {
-                    target = getHTMLElement(target.closest('.js-play-word'));
-                }
-                const path = target.dataset.src;
-                if (!path) return false;
-                this.sayWord(path);
-            });
-        });
-    }
-
-    sayWord(path: string) {
-        const audio = new Audio();
-        audio.src = `${this.data.base}/${path}`;
-        audio.autoplay = true;
-    }
-
-    showQuestion(words: IWord[], count: number) {
-        const word = words[count];
-        const qWords = words.slice();
-
-        qWords.splice(count, 1);
-        shuffle(qWords);
-        qWords.splice(4, qWords.length, word);
-        shuffle(qWords);
-
-        const choices = document.querySelectorAll('.audio__choice');
-        choices.forEach((choice, i) => {
-            choice.classList.remove('.active');
-            choice.textContent = qWords[i].wordTranslate;
-            if (qWords[i] === word) choice.setAttribute('data-answer', '');
-            else choice.removeAttribute('data-answer');
-        });
-
-        const answerPhoto = getHTMLImageElement(document.querySelector('.answer__photo'));
-        answerPhoto.src = `${this.data.base}/${word.image}`;
-
-        const answerText = getHTMLElement(document.querySelector('.answer__text'));
-        answerText.innerHTML = `<b>${word.word}</b> - ${word.wordTranslate}`;
-
-        const playBtns = document.querySelectorAll('.js-play-word');
-        playBtns.forEach((btn) => {
-            btn.setAttribute('data-src', word.audio);
-        });
-        this.sayWord(word.audio);
-    }
-
-    showResult() {
-        const main = getHTMLElement(document.querySelector('.main'));
-        main.innerHTML = '';
-        main.innerHTML = '<h2>Результаты игры</h2>';
-        //const pageResults = this.render.pageResults();
-        //const pageResults = '<h2>Результаты игры</h2>';
-        //main.append(pageResults);
-    }
-
-    showAnswer(el: HTMLElement) {
-        el.classList.add('active');
-        document.querySelector('.audio__know-btn')?.classList.add('hidden');
-        document.querySelector('.audio__next-btn')?.classList.remove('hidden');
-        document.querySelector('.audio__question')?.classList.add('hidden');
-        document.querySelector('.audio__answer')?.classList.remove('hidden');
-
-        const choices = document.querySelectorAll('.audio__choice');
-        choices.forEach((choice) => {
-            const btn = getHTMLElement(choice);
-            btn.classList.add('disabled');
-            //console.log(btn);
-        });
-    }
-
-    hideAnswer() {
-        const choices = document.querySelectorAll('.audio__choice');
-        choices.forEach((choice) => {
-            choice.classList.remove('active', 'disabled');
-        });
-        document.querySelector('.audio__know-btn')?.classList.remove('hidden');
-        document.querySelector('.audio__next-btn')?.classList.add('hidden');
-        document.querySelector('.audio__question')?.classList.remove('hidden');
-        document.querySelector('.audio__answer')?.classList.add('hidden');
+        const audioCall = new AudioCall(this.data.base, group, page);
+        audioCall.start();
     }
 
     createLogin() {
